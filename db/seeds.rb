f4c633642cc457312052b42b9ea80d27624dd622
Alexander <al.x.falconer@gmail.com>
2021-12-07 01:45:35 -0500

    updated artworks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,32 @@
 Artwork.create!([
+
   {
     name: 'Untitled (08.12.21)',
     img: 'https://i.ibb.co/xC3Tf74/Untitled-08-12-21.jpg',
     description: "Acrylic and pen on 11x14in mixed media paper.",
-    price: 70,
+    price: 140,
     category: "Paper"
   },
-
   {
-    name: 'Other Blue',
-    img: 'https://i.ibb.co/KyqLMRz/Other-Blue.jpg',
-    description: "Acrylic and pencil on 11x14in mixed media paper.",
-    price: 20,
-    category: "Paper"
+    name: 'Untitled (10.14.21)',
+    img: 'https://i.ibb.co/hfb9zcJ/Untitled-10-14-21.jpg',
+    description: "Acrylic, china marker and pencil on 11x14in frame insert.",
+    price: 140,
+    category: "Collage"
   },
-
   {
-    name: 'Nurser',
-    img: 'https://i.ibb.co/tYnYYZc/Nurser.jpg',
-    description: "Acrylic on 11x14in mixed media paper.",
-    price: 100,
-    category: "Paper"
+    name: 'Reverie',
+    img: 'https://i.ibb.co/MsM5MSR/Reverie.jpg',
+    description: "Acrylic on 30x40in canvas.",
+    price: 300,
+    category: "Canvas"
   },
 
   {
     name: "Three Women",
     img: 'https://i.ibb.co/NxGXSyp/Three-Women.jpg',
     description: "Acrylic, crayon and pencil on 11x14in mixed media paper.",
-    price: 25,
-    category: "Paper"
-  },
-
-  {
-    name: 'Haute Couture',
-    img: 'https://i.ibb.co/YywF21m/Haute-Couture.jpg',
-    description: "Acrylic and pencil on 11x14in mixed media paper.",
-    price: 70,
+    price: 120,
     category: "Paper"
   },
 
@@ -43,7 +34,7 @@ Artwork.create!([
     name: 'Maslow',
     img: 'https://i.ibb.co/YQCL5ZJ/Maslow.jpg',
     description: "Acrylic and pencil on 11x14in mixed media paper.",
-    price: 85,
+    price: 140,
     category: "Paper"
   },
 
@@ -51,7 +42,7 @@ Artwork.create!([
     name: 'Hoist that Rag',
     img: 'https://i.ibb.co/HTrQZtP/Hoist-that-Rag.jpg',
     description: "Acrylic and pencil on 11x14in mixed media paper.",
-    price: 20,
+    price: 120,
     category: "Paper"
   },
 
@@ -59,7 +50,21 @@ Artwork.create!([
     name: 'Angels for Sale',
     img: 'https://i.ibb.co/qx93Md9/Angels-for-Sale.jpg',
     description: "Acrylic and china marker on 9x12in frame insert.",
-    price: 75,
+    price: 100,
+    category: "Other"
+  },
+  {
+    name: 'Self-assembly Required',
+    img: 'https://i.ibb.co/6bgPPtT/Self-assembly-Required.jpg',
+    description: "Acrylic, ink and crayon on 8x11in printer paper",
+    price: 80,
+    category: "Collage"
+  },
+  {
+    name: 'Harmony of Pen and Sword',
+    img: 'https://i.ibb.co/tzz9k1m/Harmony-of-Pen-and-Sword.jpg',
+    description: "Acrylic on 11x14in piece of wood.",
+    price: 140,
     category: "Other"
   },
 
@@ -67,7 +72,14 @@ Artwork.create!([
     name: 'Forgetting Never Finished ',
     img: 'https://i.ibb.co/fHXqzWR/Forgetting-039-s-Never-Finished.jpg',
     description: "Acrylic and crayon on 11x14in mixed media paper.",
-    price: 70,
+    price: 120,
+    category: "Paper"
+  },
+  {
+    name: 'Feel Good Blue',
+    img: 'https://i.ibb.co/fHyHT6j/Feel-Good-Blue.jpg',
+    description: "Acrylic, pencil and crayon on 11x14in mixed media paper.",
+    price: 120,
     category: "Paper"
   },
 
@@ -75,15 +87,7 @@ Artwork.create!([
     name: 'Untitled (12.01.21)',
     img: 'https://i.ibb.co/TLqpBXF/Untitled-12-01-21.jpg',
     description: "Pencil and crayon on 11x14in mixed media paper.",
-    price: 20,
-    category: "Paper"
-  },
-
-  {
-    name: 'Feel Good Blue',
-    img: 'https://i.ibb.co/fHyHT6j/Feel-Good-Blue.jpg',
-    description: "Acrylic, pencil and crayon on 11x14in mixed media paper.",
-    price: 100,
+    price: 120,
     category: "Paper"
   },
 
@@ -91,7 +95,7 @@ Artwork.create!([
     name: "Gallows",
     img: 'https://i.ibb.co/FBwMYVD/Gallows.jpg',
     description: "Acrylic on 11x14in mixed media paper.",
-    price: 25,
+    price: 100,
     category: "Paper"
   },
 
@@ -99,63 +103,43 @@ Artwork.create!([
     name: 'Cheap Uptown Dirt',
     img: 'https://i.ibb.co/mSBRbKx/Cheap-Uptown-Dirt.jpg',
     description: "Acrylic, pencil and china marker on 9x12in paper.",
-    price: 70,
+    price: 80,
     category: "Collage"
   },
-
   {
-    name: 'Self-assembly Required',
-    img: 'https://i.ibb.co/6bgPPtT/Self-assembly-Required.jpg',
-    description: "Acrylic, ink and crayon on 8x11in printer paper",
-    price: 85,
+    name: 'Drifters',
+    img: 'https://i.ibb.co/LrmmndF/Even-Drifters-Dream-of-Home.jpg',
+    description: "Acrylic and pencil on 8x11in printer paper, digitally manipulated.",
+    price: 120,
+    category: "Digital"
+  },
+  {
+    name: 'Chitchat',
+    img: 'https://i.ibb.co/XWQ5jqy/Chitchat.jpg',
+    description: "Acrylic, pencil, color pencils on 11x14in mixed media paper.",
+    price: 120,
     category: "Collage"
   },
-
   {
-    name: 'Other Angels',
-    img: 'https://i.ibb.co/S3CbdPM/Other-Angels.jpg',
-    description: "Acrylic and china marker on 9x12in frame insert.",
-    price: 20,
-    category: "Paper"
+    name: '237 Angels (A Rose)',
+    img: 'https://i.ibb.co/f2LL7Pj/237-Angels-A-Rose.jpg',
+    description: "Acrylic, ink and pencil on 11x14 mixed media paper.",
+    price: 120,
+    category: "Canvas"
   },
-
   {
-    name: 'Harmony of Pen and Sword',
-    img: 'https://i.ibb.co/tzz9k1m/Harmony-of-Pen-and-Sword.jpg',
-    description: "Acrylic on 11x14in piece of wood.",
-    price: 75,
-    category: "Other"
+    name: 'Jarbo',
+    img: 'https://i.ibb.co/xJvFK6J/Jarbo.jpg',
+    description: "Acrylic on 11x14in mixed media paper.",
+    price: 120,
+    category: "Collage"
   },
 
   {
     name: 'Yahyah (A Major)',
     img: 'https://i.ibb.co/G7VtwmL/Yahyah-A-Major.jpg',
     description: "Acrylic, pencil and oil pastel on 14x11in mixed media paper.",
-    price: 70,
-    category: "Paper"
-  },
-
-  {
-    name: 'Untitled (10.14.21)',
-    img: 'https://i.ibb.co/hfb9zcJ/Untitled-10-14-21.jpg',
-    description: "Acrylic, china marker and pencil on 11x14in frame insert.",
-    price: 20,
-    category: "Collage"
-  },
-
-  {
-    name: 'Untitled (09.16.21)',
-    img: 'https://i.ibb.co/Jrm3xBr/Untitled-09-16-21.jpg',
-    description: "Acrylic and oil pastel on 11x14in mixed media paper.",
-    price: 100,
-    category: "Paper"
-  },
-
-  {
-    name: "Ad",
-    img: 'https://i.ibb.co/ZTNPBG3/Ad.jpg',
-    description: "Acrylic and pencil on 11x14in mixed media paper.",
-    price: 25,
+    price: 120,
     category: "Paper"
   },
 
@@ -163,50 +147,39 @@ Artwork.create!([
     name: 'Jus Ad Bellum',
     img: 'https://i.ibb.co/vqzXs9Y/Jus-Ad-Bellum.jpg',
     description: "Acrylic and pencil on 11x14in mixed media paper.",
-    price: 70,
+    price: 140,
+    category: "Paper"
+  },
+  {
+    name: 'Untitled (09.16.21)',
+    img: 'https://i.ibb.co/Jrm3xBr/Untitled-09-16-21.jpg',
+    description: "Acrylic and oil pastel on 11x14in mixed media paper.",
+    price: 200,
+    category: "Paper"
+  },
+  {
+    name: "Ad",
+    img: 'https://i.ibb.co/ZTNPBG3/Ad.jpg',
+    description: "Acrylic and pencil on 11x14in mixed media paper.",
+    price: 120,
     category: "Paper"
   },
 
   {
-    name: 'Even Drifters Dream of Home',
-    img: 'https://i.ibb.co/LrmmndF/Even-Drifters-Dream-of-Home.jpg',
-    description: "Acrylic and pencil on 8x11in printer paper, digitally manipulated.",
-    price: 85,
-    category: "Digital"
+    name: 'Other Angels',
+    img: 'https://i.ibb.co/S3CbdPM/Other-Angels.jpg',
+    description: "Acrylic and china marker on 9x12in frame insert.",
+    price: 80,
+    category: "Paper"
   },
 
   {
     name: 'Pharmacandle',
     img: 'https://i.ibb.co/Kr0mmsQ/Pharmacandle.jpg',
     description: "Acrylic, oil pastel and pencil on 30x40in canvas.",
-    price: 20,
+    price: 300,
     category: "Canvas"
   },
-
-  {
-    name: 'Candelila',
-    img: 'https://i.ibb.co/GCv3qsx/Candelila.jpg',
-    description: "Acrylic on 8x11in printer paper.",
-    price: 75,
-    category: "Paper"
-  },
-
-  {
-    name: 'Hullabaloo',
-    img: 'https://i.ibb.co/7nm2nbn/Hullabaloo.jpg',
-    description: "Acrylic, ink and pencil on 11x14in mixed media paper.",
-    price: 70,
-    category: "Collage"
-  },
-
-  {
-    name: 'Warm Memoria',
-    img: 'https://i.ibb.co/3dQrXy0/Warm-Memoria.jpg',
-    description: "Acrylic on 11x14in mixed media paper.",
-    price: 20,
-    category: "Paper"
-  },
-
   {
     name: 'Above Manhatta',
     img: 'https://i.ibb.co/Bqz4LcM/Above-Manhatta.jpg',
@@ -216,58 +189,104 @@ Artwork.create!([
   },
 
   {
+    name: 'Candelila',
+    img: 'https://i.ibb.co/GCv3qsx/Candelila.jpg',
+    description: "Acrylic on 8x11in printer paper.",
+    price: 80,
+    category: "Paper"
+  },
+
+  {
+    name: 'Hullabaloo',
+    img: 'https://i.ibb.co/7nm2nbn/Hullabaloo.jpg',
+    description: "Acrylic, ink and pencil on 11x14in mixed media paper.",
+    price: 140,
+    category: "Collage"
+  },
+
+  {
     name: "Transfiguration",
     img: 'https://i.ibb.co/gr6FNnC/Transfiguration.jpg',
     description: "Acrylic on 13x13in canvas.",
-    price: 25,
+    price: 200,
     category: "Canvas"
+  },
+  {
+    name: 'Warm Memoria',
+    img: 'https://i.ibb.co/3dQrXy0/Warm-Memoria.jpg',
+    description: "Acrylic on 11x14in mixed media paper.",
+    price: 140,
+    category: "Paper"
   },
 
   {
     name: 'Pinky"s Dream',
     img: 'https://i.ibb.co/vQgC218/Pinky-039-s-Dream.jpg',
     description: "Acrylic on 13x13in canvas.",
-    price: 70,
+    price: 200,
     category: "Canvas"
   },
 
   {
-    name: 'I Saw the Boy Break out of His Skin',
+    name: 'Untitled (11.01.21)',
+    img: 'https://i.ibb.co/HYkbj8b/Untitled-11-01-21.jpg',
+    description: "Acrylic and pencil on 11x14in mixed media paper.",
+    price: 200,
+    category: "Collage"
+  },
+
+  {
+    name: 'A Cat and Its Patti',
     img: 'https://i.ibb.co/7ySSJH5/I-Saw-the-Boy-Break-out-of-His-Skin.jpg',
     description: "Acrylic, crayon and pen on 3x5in book paper.",
-    price: 85,
+    price: 80,
     category: "Collage"
+  },
+
+  {
+    name: 'Untitled (10.28.21)',
+    img: 'https://i.ibb.co/1qL861h/80-FECC32-8-D76-4-EE7-8-F79-5340-D89-ABE81.jpg',
+    description: "Acrylic and pencil on 11x14in mixed media paper.",
+    price: 140,
+    category: "Paper"
   },
 
   {
     name: 'Conrad Moricand',
     img: 'https://i.ibb.co/DrQ83Mm/Conrad-Moricand.jpg',
     description: "Acrylic, crayon, paper on 8x11in printer paper.",
-    price: 20,
+    price: 120,
     category: "Collage"
   },
-
   {
-    name: 'Cover No Content',
-    img: 'https://i.ibb.co/vQX7qzY/Cover-No-Content.jpg',
-    description: "Wax, pastel and marker on 4x6in book cover.",
-    price: 75,
-    category: "Other"
+    name: 'Untitled (11.09.21)',
+    img: 'https://i.ibb.co/4Y18ksB/Untitled-11-09-21.jpg',
+    description: "Acrylic and pencil on 11x14in mixed media paper.",
+    price: 140,
+    category: "Paper"
   },
 
   {
     name: 'Writhe',
     img: 'https://i.ibb.co/NKQ9ZS6/Writhe.jpg',
     description: "Acrylic on 11x14in mixed media paper.",
-    price: 70,
+    price: 120,
     category: "Paper"
+  },
+
+  {
+    name: 'Cover No Content',
+    img: 'https://i.ibb.co/vQX7qzY/Cover-No-Content.jpg',
+    description: "Wax, pastel and marker on 4x6in book cover.",
+    price: 90,
+    category: "Other"
   },
 
   {
     name: 'Wing (Prayer)',
     img: 'https://i.ibb.co/vhCst4L/Wing-Prayer.jpg',
     description: "Acrylic on paper, digitally manipulated.",
-    price: 20,
+    price: 90,
     category: "Digital"
   },
 
@@ -275,18 +294,32 @@ Artwork.create!([
     name: 'Transformer',
     img: 'https://i.ibb.co/h72STjq/Transformer.jpg',
     description: "Acrylic and ink on 4x6in book paper.",
+    price: 60,
+    category: "Paper"
+  },
+  {
+    name: 'Other Blue',
+    img: 'https://i.ibb.co/KyqLMRz/Other-Blue.jpg',
+    description: "Acrylic and pencil on 11x14in mixed media paper.",
     price: 100,
     category: "Paper"
   },
-
+  {
+    name: 'Haute Couture',
+    img: 'https://i.ibb.co/YywF21m/Haute-Couture.jpg',
+    description: "Acrylic and pencil on 11x14in mixed media paper.",
+    price: 120,
+    category: "Paper"
+  },
 
   {
-    name: 'Reverie',
-    img: 'https://i.ibb.co/MsM5MSR/Reverie.jpg',
-    description: "Acrylic on 30x40in canvas.",
+    name: 'Nurser',
+    img: 'https://i.ibb.co/tYnYYZc/Nurser.jpg',
+    description: "Acrylic on 11x14in mixed media paper.",
     price: 100,
-    category: "Canvas"
+    category: "Paper"
   }
+
 
 ])
 
